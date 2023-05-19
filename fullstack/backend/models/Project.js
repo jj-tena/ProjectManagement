92f70080969558b51249a9c3c76e3efce7e96265
josejustotena0@gmail.com <josejustotena0@gmail.com>
2023-05-19 17:42:55 +0200

    Added model for Project

---
+++ fullstack/backend/models/Project.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import User from "./User";
+
+const projectSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    }, 
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    }, 
+    launchDate: {
+        type: Date,
+        default: Date.now(),
+    }, 
+    client: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    collaborators: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }
+}, {
+    timestamps: true,
+});
+
+const Project = mongoose.model('Project', projectSchema);
+export default Project;
