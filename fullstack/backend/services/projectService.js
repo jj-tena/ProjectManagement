747a026e5c18de770251cc485313c19323efbd8e
josejustotena0@gmail.com <josejustotena0@gmail.com>
2023-05-24 09:51:37 +0200

    Implemented all methods for task controller and service

--- fullstack/backend/services/projectService.js
+++ fullstack/backend/services/projectService.js
@@ -1,4 +1,5 @@
 import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
 const getProjects = async (req, res) => {
     const projects = await Project.find().where('creator').equals(req.user._id);
@@ -10,7 +11,7 @@ const createProject = async (req, res) => {
     project.creator = req.user._id;
     try {
         const projectSaved = await project.save();
-        res.status(200).json(projectSaved);
+        return res.status(200).json(projectSaved);
     } catch (error) {
         return res.status(400).json({msg: error.message});
     }
@@ -78,7 +79,21 @@ const deleteCollaborator = async (req, res) => {
 };
 
 const getTasks = async (req, res) => {
-
+    const {id} = req.params;
+    const project = await Project.findById(id);
+    if (!project) {
+        return res.status(404).json({msg: 'Project not found'});
+    }
+    if (project.creator.toString() !== req.user._id.toString()) {
+        const error = new Error('Action not allowed');
+        return res.status(401).json({msg: error.message});
+    }
+    try {
+        const tasks = await Task.find().where("project").equals(id)
+        return res.json(tasks);
+    } catch (error) {
+        return res.status(400).json({msg: error.message});
+    }
 };
 
 export {
--- fullstack/backend/services/taskService.js
+++ fullstack/backend/services/taskService.js
@@ -1,10 +1,82 @@
-const addTask = async (req, res) => {};
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
-const getTask = async (req, res) => {};
+const addTask = async (req, res) => {
+    const {project} = req.body;
+    const projectFound = await Project.findById(project);
+    if (!projectFound) {
+        const error = new Error("Project not found");
+        return res.status(404).json({msg: error.message});
+    }
+    if (projectFound.creator.toString() !== req.user._id.toString()) {
+        const error = new Error("You are not allowed to create tasks for this project");
+        return res.status(403).json({msg: error.message}); 
+    }
+    try {
+        const task = await Task.create(req.body);
+        return res.status(200).json(task);
+    } catch (error) {
+        return res.status(404).json({msg: error.message});
+    }
+};
 
-const updateTask = async (req, res) => {};
+const getTask = async (req, res) => {
+    const {id} = req.params;
+    const task = await Task.findById(id).populate("project");
+    if (!task) {
+        const error = new Error("Task not found");
+        return res.status(404).json({msg: error.message});
+    }
+    if (task.project.creator.toString() !== req.user._id.toString()){
+        const error = new Error("You are not allowed to get this task");
+        return res.status(403).json({msg: error.message});
+    }
+    return res.status(200).json(task);
+};
 
-const deleteTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+    const {id} = req.params;
+    const task = await Task.findById(id).populate("project");
+    if (!task) {
+        const error = new Error("Task not found");
+        return res.status(404).json({msg: error.message});
+    }
+    if (task.project.creator.toString() !== req.user._id.toString()){
+        const error = new Error("You are not allowed to get this task");
+        return res.status(403).json({msg: error.message});
+    }
+    task.name = req.body.name || task.name;
+    task.description = req.body.description || task.description;
+    task.priority = req.body.priority || task.priority;
+    task.launchDate = req.body.launchDate || task.launchDate;
+    try {
+        const taskUpdated = await task.save();
+        return res.status(200).json(taskUpdated);
+
+    } catch (error) {
+        return res.status(400).json({msg: error.message});
+    }
+};
+
+const deleteTask = async (req, res) => {
+    const {id} = req.params;
+    const task = await Task.findById(id).populate("project");
+    if (!task) {
+        const error = new Error("Task not found");
+        return res.status(404).json({msg: error.message});
+    }
+    if (task.project.creator.toString() !== req.user._id.toString()){
+        const error = new Error("You are not allowed to get this task");
+        return res.status(403).json({msg: error.message});
+    }
+    try {
+        await task.deleteOne();
+        return res.status(200).json({msg: "Task deleted"});
+
+    } catch (error) {
+        return res.status(400).json({msg: error.message});
+    }
+};
 
 const updateState = async (req, res) => {};
 
