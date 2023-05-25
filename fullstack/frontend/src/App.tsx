fbf16cbdb74202d54c56b1567305fd21ba4aa5b8
josejustotena0@gmail.com <josejustotena0@gmail.com>
2023-05-25 09:49:03 +0200

    Added react-router-dom and set up routes

--- fullstack/frontend/src/App.tsx
+++ fullstack/frontend/src/App.tsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AuthLayout from "./layout/AuthLayout"
+import ForgotPassword from "./views/ForgotPassword"
+import Login from "./views/Login"
+import Register from "./views/Register"
+import ResetPassword from "./views/ResetPassword"
+import VerifyAccount from "./views/VerifyAccount"
+
 const App = () => {
 
   return (
-    <>
-      <div>
-        <h1>Hello world</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="forgot-password/:token" element={<ResetPassword />} />
+          <Route path="verify-account/:id" element={<VerifyAccount />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
